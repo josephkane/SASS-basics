9f16fef693b1d74742f58f330b32d313459b4842
Joseph Kane <josephckane@gmail.com>
2016-05-25 10:30:47 -0500

    learning how to create and set up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,34 +4,70 @@ const gulp = require("gulp");
 const sass = require("gulp-sass");
 const jshint = require("gulp-jshint");
 const sassLint = require('gulp-sass-lint');
+const del = require("del");
 
+const sourcePath = "./src";
+const distributionPath = "./dist";
+const jsPath = `${sourcePath}/**/*.js`;
+const sassPath = `${sourcePath}/**/*.scss`;
+const staticPath = ["${sourcePath}/**/*", `!${sassPath}`];
+
+gulp.task("default", ["build"]);
+gulp.task("build", ["clean", "static:copy", "sass:compile"]);
+gulp.task("watch", ["static:watch", "sass:watch", "js:watch"]);
+gulp.task("lint", ["js:lint", "sass:lint"]);
+
+
+// UTILITIES
+gulp.task("clean", () => (
+  del(`${distributionPath}/**/*`)
+));
+
+
+// STATIC
+gulp.task("static:copy", ["clean"], () => (
+  gulp.src(["${sourcePath}/**/*", `!${sassPath}`])
+    .pipe(gulp.dest(distributionPath))
+));
+
+gulp.task("static:watch", ["clean"], () => (
+  gulp.src(staticPath, ["static:copy"])
+));
+
+
+// SASS
+
+const sassCompile = () => {
+  gulp.src(sassPath)
+    .pipe(sass())
+    .pipe(gulp.dest(distributionPath))
+};
 
 gulp.task("sass:watch", () => (
-	gulp.watch("./static/**/*.scss",
+	gulp.watch(sassPath,
 		["sass:lint", "sass:compile"]
 	)
 ));
 
-gulp.task("sass:compile", () => (
-	gulp.src("./static/**/*.scss")
-		.pipe(sass())
-		.pipe(gulp.dest("./static"))
-));
+gulp.task("sass:compileAndClean", ["clean"], sassCompile);
 
+gulp.task("sass:compile", sassCompile);
 
 gulp.task("sass:lint", () => (
-  gulp.src('static/**/*.scss')
+  gulp.src(sassPath)
     .pipe(sassLint())
     .pipe(sassLint.format())
     .pipe(sassLint.failOnError())
 ));
 
+
+// JS
+gulp.task("js:watch", () => (
+  gulp.watch(jsPath, ["js:lint"])
+));
+
 gulp.task("js:lint", () => (
-  gulp.src('./static/**/*.js')
+  gulp.src(jsPath)
     .pipe(jshint())
     .pipe(jshint.reporter("jshint-stylish"))
 ));
-
-gulp.task("sass", ["sass:lint", "sass:compile", "sass:watch"]);
-gulp.task("lint", ["js:lint", "sass:lint"]);
-gulp.task("default", ["sass"]);
